7705dffbe0b3e056d00c4fa7ceb50980a553f1c0
minhaz <minhazav@gmail.com>
2017-01-17 20:51:28 +0530

    Tested decision tree with different criterion, updated result

--- classifiers/github_labels/decision_tree/classifier.py
+++ classifiers/github_labels/decision_tree/classifier.py
@@ -14,21 +14,21 @@ ratio = 90
 print ("loading dataset %s from files" % dataset_name)
 dataset = loadData(dataset_name, False)
 labels = loadData(dataset_name, True)
+reduceDatasetBy = 5
 
- 
 print ("dataset and labels loaded, splitting using cross_validation")
 features_train, features_test, labels_train, labels_test = preprocess(dataset, labels, 1 - (ratio / 100))
  
  
-features_train = features_train[:int(len(features_train) / 5)]
-labels_train = labels_train[:int(len(labels_train) / 5)]
+features_train = features_train[:int(len(features_train) / reduceDatasetBy)]
+labels_train = labels_train[:int(len(labels_train) / reduceDatasetBy)]
  
 print ("Size of training data = %s" % len(labels_train))
 print ("Size of test data = %s" % len(labels_test))
 
 print ("training classifier using decision tree")
 _start = time.time()
-clf = tree.DecisionTreeClassifier(min_samples_split=500)
+clf = tree.DecisionTreeClassifier(min_samples_split=500, criterion="entropy")
 clf.fit(features_train, labels_train)
 _training_time = time.time() - _start
 _start = time.time()
