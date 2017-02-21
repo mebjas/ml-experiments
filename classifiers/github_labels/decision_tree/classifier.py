bfc6a5fa0b0d9a8e237f4ce3303530d1693506ff
minhaz <minhazav@gmail.com>
2017-02-21 23:37:38 +0530

    Added updated classifier codes and reports
    primarily for syncing work on powerful device. Unable to perform
    GridSearchCV on self machine.

--- classifiers/github_labels/decision_tree/classifier.py
+++ classifiers/github_labels/decision_tree/classifier.py
@@ -3,18 +3,29 @@ import pickle
 import time
 from sklearn import tree
 from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import numpy as np
 
 sys.path.append("../../../libs")
 from dataset import loadData
 from datapreprocessing import preprocess
 
+# variables
 dataset_name = "github-issue-bug-enhancement"
 ratio = 90
+min_samples_split = 500
+criterion="entropy"
+
+# Create color maps
+cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
+cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
  
 print ("loading dataset %s from files" % dataset_name)
 dataset = loadData(dataset_name, False)
 labels = loadData(dataset_name, True)
-reduceDatasetBy = 5
+reduceDatasetBy = 100
+
 
 print ("dataset and labels loaded, splitting using cross_validation")
 features_train, features_test, labels_train, labels_test = preprocess(dataset, labels, 1 - (ratio / 100))
@@ -28,7 +39,7 @@ print ("Size of test data = %s" % len(labels_test))
 
 print ("training classifier using decision tree")
 _start = time.time()
-clf = tree.DecisionTreeClassifier(min_samples_split=500, criterion="entropy")
+clf = tree.DecisionTreeClassifier(min_samples_split=min_samples_split, criterion=criterion)
 clf.fit(features_train, labels_train)
 _training_time = time.time() - _start
 _start = time.time()
@@ -39,4 +50,3 @@ _prediciton_time = time.time() - _start
  
 print ("training size - %s%%, test size - %s%% on dataset `%s`" % (ratio, 100 - ratio, dataset_name))
 print ("Accuracy - %s, Classification Time - %s, Prediciton Time - %s" % (accuracy_score(result, labels_test), _training_time, _prediciton_time))
- 
--- classifiers/github_labels/randomforest/classifier.py
+++ classifiers/github_labels/randomforest/classifier.py
@@ -2,8 +2,9 @@ import sys
 import pickle
 import time
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support, classification_report
 from sklearn.feature_selection import SelectPercentile, f_classif
+from sklearn.model_selection import GridSearchCV
 
 sys.path.append("../../../libs")
 from dataset import loadData
@@ -15,7 +16,7 @@ ratio = 90
 print ("loading dataset %s from files" % dataset_name)
 dataset = loadData(dataset_name, False)
 labels = loadData(dataset_name, True)
-reduceDatasetBy = 1
+reduceDatasetBy = 10
 
 print ("dataset and labels loaded, splitting using cross_validation")
 features_train, features_test, labels_train, labels_test = preprocess(dataset, labels, 1 - (ratio / 100))
@@ -30,24 +31,37 @@ print ("Size of test data = %s" % len(labels_test))
 print ("performing automated feature seleciton")
 print ("No of features before selection = %d" % len(features_train[0]))
 
-selector = SelectPercentile(f_classif, percentile=50)
+selector = SelectPercentile(f_classif, percentile=95)
 selector.fit(features_train, labels_train)
-features_train = selector.transform(features_train) #.toarray()
-features_test = selector.transform(features_test) #.toarray()
+features_train = selector.transform(features_train)
+features_test = selector.transform(features_test)
 print ("No of features after selection = %d" % len(features_train[0]))
-
 
 print ("training classifier using random forest ensemble")
 _start = time.time()
-clf = RandomForestClassifier(n_estimators=15, criterion="entropy")
+
+
+# Applying grid GridSearchCV
+clf_rfc = RandomForestClassifier()
+params = {'n_estimators': [5, 10, 20, 50, 100, 200] ,'criterion': ['entropy', 'gini']}
+# clf = RandomForestClassifier(n_estimators=15, criterion="entropy")
+clf = GridSearchCV(clf_rfc, params)
 clf.fit(features_train, labels_train)
 _training_time = time.time() - _start
 _start = time.time()
+
+print ("best params")
+print(clf.best_params_)
  
 print ("predicting test data")
 result = clf.predict(features_test)
 _prediciton_time = time.time() - _start
+
+precision, recall, fbetascore, support  = precision_recall_fscore_support(labels_test, result)
  
 print ("training size - %s%%, test size - %s%% on dataset `%s`" % (ratio, 100 - ratio, dataset_name))
 print ("Accuracy - %s, Classification Time - %s, Prediciton Time - %s" % (accuracy_score(result, labels_test), _training_time, _prediciton_time))
- 
+print ("Precision: %s, Recall: %s, F1_Score: %s" % (precision, recall, fbetascore))
+
+print ("____ report ______")
+print (classification_report(labels_test, result))
